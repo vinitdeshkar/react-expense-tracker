75925498d23395de22f6cb382ccbcd74283a4820
Vinit Deshkar <vinit.deshkar@autodesk.com>
2021-09-11 20:32:37 +0530

    Lifting the state up and added some new components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,16 @@
-import './ExpenseItem.css';
-import Card from '../UI/Card';
+import React from "react";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </Card>
   );
